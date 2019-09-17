85d240f0e49418b8a07424c3dd41ecf3be6e98e2
Wayne Sun <wtse.sun@gmail.com>
2019-09-16 20:52:35 -0700

    Implemented nightmare async function to retrieve the price on Amazon

--- tracker.js
+++ tracker.js
@@ -1,8 +1,17 @@
-const nightmare = require('nightmare');
+const nightmare = require('nightmare')();
 
-"priceblock_ourprice"
-const url = "https://www.amazon.com/dp/B0756CYWWD/ref=twister_B075RYG44J"
+// "priceblock_ourprice"
+const URL = "https://www.amazon.com/dp/B0756CYWWD/ref=twister_B075RYG44J";
+const TARGET_PRICE = 200;
 
 async function checkPrice() {
-  
+  console.log("Loading Price...")
+  const priceString = await nightmare.goto(URL)
+                                     .wait("#priceblock_ourprice")
+                                     .evaluate(() => document.getElementById("priceblock_ourprice").innerText)
+                                     .end();
+  const priceNumber = parseFloat(priceString.replace("$", ""));
+  console.log(priceNumber);
 }
+
+checkPrice();
