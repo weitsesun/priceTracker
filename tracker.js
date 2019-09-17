94b092f182069452259a181ff378d60b4ba881db
Wayne Sun <wtse.sun@gmail.com>
2019-09-16 21:24:01 -0700

    Sent email to tempemail service

--- tracker.js
+++ tracker.js
@@ -7,7 +7,7 @@ const nightmare = require('nightmare')();
 const args = process.argv.slice(2);
 
 const URL = args[0] || "https://www.amazon.com/dp/B0756CYWWD/ref=twister_B075RYG44J";
-const TARGET_PRICE = args[1] || 200;
+const TARGET_PRICE = args[1] || 500;
 
 checkPrice();
 
@@ -20,9 +20,31 @@ async function checkPrice() {
   const priceNumber = parseFloat(priceString.replace("$", ""));
 
   if(priceNumber <= TARGET_PRICE) {
-    console.log("BUY IT NOW!");
+    sendEmail(
+      'Price is Low',
+      `The price on ${URL} has dropped below ${TARGET_PRICE}`,
+    )
   } else {
     console.log("It is expensive");
   }
 }
 
+
+/**
+ * sendEmail()
+ * This function will automatically send an email to the receiver if the
+ * price is lower than the target price
+ * @param: subject(string), body(string)
+ */
+
+ function sendEmail(subject, body) {
+  const email = {
+    to: "joviyogo@idea-mail.net",
+    from: 'amazon-price-checker@example.com',
+    subject: subject,
+    text: body,
+    html: body
+  }
+
+  return sgMail.send(email);
+ };
